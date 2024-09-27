ee96c56261b5b2abd8416ccec6b81f5a75ee49ba
Thegeekpod <supriyo7dey@gmail.com>
2024-09-27 15:12:04 +0530

    cc

--- app/api/frontend/Product/random/route.js
+++ app/api/frontend/Product/random/route.js
@@ -6,17 +6,7 @@ const prisma = new PrismaClient();
 export async function GET(req) {
   noStore();
   try {
-    // Count the total number of active products
-    const totalProducts = await prisma.product.count({
-      where: {
-        status: 'ACTIVE',
-      },
-    });
-
-    // Generate a random skip value to fetch random products
-    const randomSkip = Math.floor(Math.random() * (totalProducts - 10));
-
-    // Fetch random 10 active products with their related category information
+ 
     const products = await prisma.product.findMany({
       where: {
         status: 'ACTIVE', // Filter for active products
@@ -27,17 +17,10 @@ export async function GET(req) {
       orderBy: {
         id: 'desc',
       },
-      take: 14, // Limit to 10 products
-      skip: randomSkip > 0 ? randomSkip : 0, // Skip a random number of products to fetch random ones
     });
 
-    // Map the products to include category name
-    const productsWithCategoryName = products.map((product) => ({
-      ...product,
-      category_name: product.category.name, // Add category name to the product
-    }));
 
-    return new Response(JSON.stringify(productsWithCategoryName), {
+    return new Response(JSON.stringify(products), {
       status: 200,
     });
   } catch (error) {
